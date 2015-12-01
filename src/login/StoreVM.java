818061de85655ac1b48a33bd1fd66ae8182f6372
Thierry van der Kolk <thierryvdkolk@outlook.comgit config --global user.email thierry.van.der.kolk@hva.nl>
2015-12-01 09:17:36 +0100

    sss

--- src/login/StoreVM.java
+++ src/login/StoreVM.java
@@ -1,7 +1,7 @@
 package login;
 
 import java.io.IOException;
-import java.io.PrintWriter;
+import java.sql.*;
 import javax.swing.*;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -42,16 +42,18 @@ public class StoreVM extends HttpServlet {
 		// TODO Auto-generated method stub
 		doGet(request, response);
 		
-		//Connect database
-		Database dbcon = new Database();
-		Connection conn = dbcon.returnConnection();
-		stmt = conn.createStatement();
 		//Declare some things.
 		HttpSession session = request.getSession(false);
 		String user = (String) session.getAttribute("username");
-		PrintWriter out = response.getWriter();
 		final JDialog dialog = new JDialog();
-		dialog.setAlwaysOnTop(true);    
+		dialog.setAlwaysOnTop(true);  
+		
+		try { 
+		//Connect database
+		Database dbcon = new Database();
+		Connection con = dbcon.returnConnection();
+		Statement statement = con.createStatement();
+		
 		
 		if (JOptionPane.showConfirmDialog(dialog, "Are you sure?", "WARNING",
 		        JOptionPane.YES_NO_OPTION) == JOptionPane.YES_OPTION) {
@@ -66,30 +68,36 @@ public class StoreVM extends HttpServlet {
 			} else {
 				
 				if(button == 1) {
-					String sql = "INSERT INTO vms (cpu, ram, storage, status) VALUES (1,2,50,running) WHERE email = "+ user ;
-					stmt.executeUpdate(sql);
-					if (update > 0 ) {
+					String sql = "INSERT INTO vms (cpu, ram, storage, status) VALUES (1,2,50,running) WHERE email = "+ user +"";
+					int updated = statement.executeUpdate(sql);
+					if (updated > 0 ) {
 					JOptionPane.showMessageDialog(dialog, "Succesfull registered a VM!.");
+					RequestDispatcher rs = request.getRequestDispatcher("hosting.jsp");
+					rs.forward(request, response);		
 					} else {
 					JOptionPane.showMessageDialog(dialog, "Something went wrong! Please contact us.");
 					RequestDispatcher rs = request.getRequestDispatcher("index.html");
 					rs.forward(request, response);
 					}
 				} else if (button == 2) {
-					String sql = "INSERT INTO vms (cpu, ram, storage, status) VALUES (2,4,200,running) WHERE email = "+ user ;
-					int updated = stmt.executeUpdate(sql);
-					if (update > 0) {
+					String sql = "INSERT INTO vms (cpu, ram, storage, status) VALUES (2,4,200,running) WHERE email = "+ user +"";
+					int updated = statement.executeUpdate(sql);
+					if (updated > 0) {
 					JOptionPane.showMessageDialog(dialog, "Succesfull registered a VM!.");
+					RequestDispatcher rs = request.getRequestDispatcher("hosting.jsp");
+					rs.forward(request, response);		
 					} else {
 					JOptionPane.showMessageDialog(dialog, "Something went wrong. Please contact us");
 					RequestDispatcher rs = request.getRequestDispatcher("index.html");
 					rs.forward(request, response);						
 						}
 				} else if (button == 3) {
-					String sql = "INSERT INTO vms (cpu, ram, storage, status) VALUES (4,8,500,running) WHERE email = "+ user ;
-					int update = stmt.executeUpdate(sql);
+					String sql = "INSERT INTO vms (email, cpu, ram, storage, status) VALUES (thierry,4,8,500,running)";
+					int update = statement.executeUpdate(sql);
 					if (update > 0) { 
 					JOptionPane.showMessageDialog(dialog, "Succesfull registered a VM!.");
+					RequestDispatcher rs = request.getRequestDispatcher("hosting.jsp");
+					rs.forward(request, response);		
 					} else {
 					JOptionPane.showMessageDialog(dialog, "Something went wrong. Please contact us");	
 					RequestDispatcher rs = request.getRequestDispatcher("index.html");
@@ -105,6 +113,9 @@ public class StoreVM extends HttpServlet {
 			JOptionPane.showMessageDialog(dialog, "You now will be redirected to the homepage.");
 			RequestDispatcher rs = request.getRequestDispatcher("index.html");
 			rs.forward(request, response);
+		}
+		} catch (Exception e ) {
+			e.printStackTrace();
 		}
 		
 }
