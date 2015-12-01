b421a796ad14c8ada761d00daea2f1ef434a1338
Thierry van der Kolk <thierryvdkolk@outlook.comgit config --global user.email thierry.van.der.kolk@hva.nl>
2015-12-01 11:31:30 +0100

    Update

---
+++ src/login/Login.java
@@ -0,0 +1,60 @@
+package login;
+ 
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+import javax.servlet.http.HttpSession;
+import javax.swing.*;
+/***
+ * Servlet implementation class Login
+ */
+ 
+@WebServlet("/login")
+public class Login extends HttpServlet {
+ 
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 1L;
+ 
+    //Called by the server (via the service method) to allow a servlet to handle a POST request.
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+    //gives an exception to the servlet which it can throw it encounters difficulty. Problem I/O is with reading/writing data from url. 
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        
+        //Declare some things
+        final JDialog dialog = new JDialog();
+        dialog.setAlwaysOnTop(true);    
+         
+        String emaillogin = request.getParameter("emaillogin");
+        String passlogin = request.getParameter("passlogin");
+        
+        //asks the Validate.checkUser if the login is valid. 
+        if(Validate.checkUser(emaillogin, passlogin))
+        {
+        	HttpSession session = request.getSession();
+        	session.setAttribute("username", emaillogin);
+        	session.setMaxInactiveInterval(30*60);
+        	
+        //Name and value of the cookie.
+            Cookie ck=new Cookie("email",emaillogin);
+            response.addCookie(ck);
+        //Cookie stays 30 minutes.
+            ck.setMaxAge(1800);
+        //Request the welcome.java file and forward client to that file.
+            JOptionPane.showMessageDialog(dialog, "Successfull logged in!");
+            RequestDispatcher rs = request.getRequestDispatcher("index.html");
+            rs.forward(request, response);
+        }
+        else
+        {
+        //User isnt in database. Then show message and send again to login.html with clean fields.
+        	JOptionPane.showMessageDialog(dialog, "Username or password incorrect!");
+           RequestDispatcher rs = request.getRequestDispatcher("index.html");
+           rs.forward(request, response);
+        }
+        
+    }  
+}
---
+++ src/login/StoreVM.java
@@ -0,0 +1,144 @@
+package login;
+
+import java.io.IOException;
+import java.sql.*;
+import java.sql.PreparedStatement;
+import javax.swing.*;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class storeVM
+ */
+@WebServlet("/storeVM")
+public class StoreVM extends HttpServlet {
+	
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public StoreVM() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+		
+		//Declare some things.
+		HttpSession session = request.getSession(false);
+		String user = (String) session.getAttribute("username");
+		final JDialog dialog = new JDialog();
+		dialog.setAlwaysOnTop(true);  
+		
+		try { 
+		//Connect database
+		Database dbcon = new Database();
+		Connection con = dbcon.returnConnection();
+		PreparedStatement ps = con.prepareStatement("INSERT INTO vms VALUES(?,?,?,?,?)");
+		
+		if (JOptionPane.showConfirmDialog(dialog, "Are you sure?", "WARNING",
+		        JOptionPane.YES_NO_OPTION) == JOptionPane.YES_OPTION) {
+			//Make some variables and shorter things.
+			int button = Integer.parseInt(request.getParameter("vmstore"));
+
+			//Check if session is set.
+			if(session == null || session.getAttribute("username") == null) {
+				JOptionPane.showMessageDialog(dialog, "Please login first before ordering.");
+				RequestDispatcher rs = request.getRequestDispatcher("index.html");
+	            rs.forward(request, response);
+			} else {
+				
+				if(button == 1) {
+					ps.setString(1, user);
+					ps.setString(2, "running");
+					ps.setInt(3, 1);
+					ps.setInt(4, 2);
+					ps.setInt(5, 50);
+					int updated =ps.executeUpdate();
+					if (updated > 0 ) {
+					JOptionPane.showMessageDialog(dialog, "Succesfull registered a VM!.");
+					RequestDispatcher rs = request.getRequestDispatcher("hosting.jsp");
+					rs.forward(request, response);		
+					} else {
+					JOptionPane.showMessageDialog(dialog, "Something went wrong! Please contact us.");
+					RequestDispatcher rs = request.getRequestDispatcher("index.html");
+					rs.forward(request, response);
+					}
+				} else if (button == 2) {
+					ps.setString(1, user);
+					ps.setString(2, "running");
+					ps.setInt(3, 1);
+					ps.setInt(4, 2);
+					ps.setInt(5, 50);
+					int updated =ps.executeUpdate();
+					if (updated > 0) {
+					JOptionPane.showMessageDialog(dialog, "Succesfull registered a VM!.");
+					RequestDispatcher rs = request.getRequestDispatcher("hosting.jsp");
+					rs.forward(request, response);		
+					} else {
+					JOptionPane.showMessageDialog(dialog, "Something went wrong. Please contact us");
+					RequestDispatcher rs = request.getRequestDispatcher("index.html");
+					rs.forward(request, response);						
+						}
+				} else if (button == 3) {
+					ps.setString(1, user);
+					ps.setString(2, "running");
+					ps.setInt(3, 1);
+					ps.setInt(4, 2);
+					ps.setInt(5, 50);
+					int updated =ps.executeUpdate();
+					if (updated > 0) { 
+					JOptionPane.showMessageDialog(dialog, "Succesfull registered a VM!.");
+					RequestDispatcher rs = request.getRequestDispatcher("hosting.jsp");
+					rs.forward(request, response);		
+					} else {
+					JOptionPane.showMessageDialog(dialog, "Something went wrong. Please contact us");	
+					RequestDispatcher rs = request.getRequestDispatcher("index.html");
+					rs.forward(request, response);
+					}
+				} else {
+					JOptionPane.showMessageDialog(dialog, "Something went wrong. Please contact us.");
+					RequestDispatcher rs = request.getRequestDispatcher("index.html");
+					rs.forward(request, response);
+				
+				
+		}}} else {
+			JOptionPane.showMessageDialog(dialog, "You now will be redirected to the homepage.");
+			RequestDispatcher rs = request.getRequestDispatcher("index.html");
+			rs.forward(request, response);
+		}
+		} catch (Exception e ) {
+			e.printStackTrace();
+		}
+		
+}
+		
+	}
+	
+
+
+
+
+
+
+
+
